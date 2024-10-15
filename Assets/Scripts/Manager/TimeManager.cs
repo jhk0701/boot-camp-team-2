2cf88b9c4d3644c34fd230c33d0a34f95f0f6d24
Gido Park <111720411+prayer0420@users.noreply.github.com>
2024-10-15 16:42:24 +0900

    Add GameManager,TimeManager, State(Lobby, GameScene, Lose, Win, Pasue)

---
+++ Assets/Scripts/Manager/TimeManager.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+
+public class TimeManager : MonoBehaviour
+{
+    public float maxTime = 60f; 
+    private float remainTime;
+    private float elapsedTime;
+    private bool isPlaying;
+
+
+    public void StartTimer()
+    {
+        elapsedTime = 0f;
+        remainTime = maxTime;
+        isPlaying = true;
+    }
+
+    public void StopTimer()
+    {
+        isPlaying = false;
+    }
+
+    public void PauseTimer()
+    {
+        isPlaying = false;
+    }
+
+    public void ResumeTimer()
+    {
+        isPlaying = true;
+    }
+
+    private void Update()
+    {
+        if (isPlaying)
+        {
+            remainTime -= Time.deltaTime;
+            
+            elapsedTime += Time.deltaTime;
+        }
+    }
+
+    public float GetRemainingTime()
+    {
+        return remainTime;
+    }
+
+    public float GetElaspedTime()
+    {
+        return elapsedTime;
+    }
+
+    public bool CheckRemainTime()
+    {
+        return remainTime <= 0 ? true : false;
+    }
+}
---
+++ Assets/Scripts/State/LobbyState.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LobbyState : IGameState
+{
+   
+    private GameManager gameManager;
+    public LobbyState(GameManager gameManager)
+    {
+        this.gameManager = gameManager;
+    }
+
+
+    public void EnterState()
+    {
+        //UI, Sound È°¼ºÈ­
+    }
+    public void UpdateState()
+    {
+    }
+
+    public void ExitState()
+    {
+        gameManager.timeManager.StartTimer();
+    }
+
+}
