e97df179ae952123a780d787aaef5dbdd908cd7a
hi3816 <wlgp2534@naver.com>
2024-10-16 19:39:32 +0900

    UPD loseDisplay

--- Assets/Scripts/UI/GameUIManager.cs
+++ Assets/Scripts/UI/GameUIManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class GameUIManager : MonoBehaviour
@@ -15,9 +16,15 @@ public class GameUIManager : MonoBehaviour
     private float initialTime = 0f;
     private int initialScore = 0;
     private int initialLives = 3;
+
+    [SerializeField] Button retryButton;
+    [SerializeField] GameObject loseGamePanel;
+
     void Start()
     {
         InitializeUI();
+        loseGamePanel.SetActive(false); // 초기에는 비활성화
+        retryButton.onClick.AddListener(RetryGame);
     }
 
     // UI 초기화 메서드
@@ -28,9 +35,22 @@ public class GameUIManager : MonoBehaviour
 
     public void UpdateUI(int level, float playTime, int score, int lives)
     {
+
         levelText.text = level.ToString();
         timeText.text = playTime.ToString("F2");
         scoreText.text = score.ToString();
         livesText.text = lives.ToString();
+    }
+
+    public void ShowLoseGameUI()
+    {
+        loseGamePanel.SetActive(true); // 게임 오버 UI 활성화
+    }
+
+    public void RetryGame()
+    {
+        // 게임을 재시작하는 로직
+        // 예: 현재 씬 재로드
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
 }
