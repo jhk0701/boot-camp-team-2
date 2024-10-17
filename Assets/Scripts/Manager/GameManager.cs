5af3e93936e7cc26bd8d90eebacd2308ca979e8f
Gido Park <111720411+prayer0420@users.noreply.github.com>
2024-10-17 12:06:24 +0900

    Update Managers Event

--- Assets/Scripts/Manager/GameManager.cs
+++ Assets/Scripts/Manager/GameManager.cs
@@ -1,33 +1,18 @@
 using UnityEngine;
-using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
-    public static GameManager Instance; 
+    public static GameManager Instance;
 
-    public enum GameState
-    {
-        Lobby,
-        GameScene,
-        Pause,
-        Win,
-        Lose
-    }
-
-    public GameState CurrentState { get; private set; }
-
-    public delegate void StateChanged(GameState newState);
-    public event StateChanged OnStateChanged; // 상태 변경 이벤트
-
-    private BrickManager brickManager;
-
+    [SerializeField] private int playerLives = 3;
+    private int score = 0;
 
     private void Awake()
     {
         if (Instance == null)
         {
             Instance = this;
-            DontDestroyOnLoad(gameObject); 
+            DontDestroyOnLoad(gameObject);
         }
         else
         {
@@ -37,31 +22,40 @@ public class GameManager : MonoBehaviour
 
     private void Start()
     {
-        SetState(GameState.Lobby);
+        //처음에 Start상태로 변환(알림)
+        StateManager.Instance.SetState(StateManager.GameState.Start);
 
-        brickManager = FindObjectOfType<BrickManager>();
+        // 벽돌 이벤트 구독
+        BrickManager brickManager = FindObjectOfType<BrickManager>();
         if (brickManager != null)
         {
             brickManager.OnAllBrickBroken += HandleAllBricksBroken;
         }
     }
 
-    public void SetState(GameState newState)
-    {
-        CurrentState = newState;
-        OnStateChanged?.Invoke(newState); 
-    }
-
 
     private void HandleAllBricksBroken()
     {
-        SetState(GameState.Win);
+        // 모든 벽돌이 부서지면 승리 상태로 전환
+        StateManager.Instance.SetState(StateManager.GameState.Win);
+        Debug.Log("Game Won!");
     }
 
-    public void StartGameScene()
+    //TODO: BallController에서 이벤트 정의, 알림 후 구독
+    private void HandleLifeLost()
     {
-        SetState(GameState.GameScene);
-        SceneManager.LoadScene(1);
+        Debug.Log("Life Lost!");
     }
 
+    private void HandleAllLivesLost()
+    {
+        playerLives--;
+        // 모든 목숨을 잃으면 패배 상태로 전환
+        if (playerLives <= 0)
+        {
+            StateManager.Instance.SetState(StateManager.GameState.Lose);
+            Debug.Log("Game Over - No Lives Left");
+        }
+    }
 }
+
